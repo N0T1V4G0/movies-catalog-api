c8852b1f1dba09cad4f4a7f895855397945d9c32
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-12-06 01:12:41 -0300

    chore: add app entrypoint

---
+++ cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	//set up app config
+	var app application
+	app.Domain = "example.com"
+
+	// read from cli
+
+	// connect to db
+
+	// start api
+	log.Println("starting web server at port", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
