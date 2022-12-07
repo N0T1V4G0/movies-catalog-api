8f48c52083273d07e3e5148f76a13d28c7934556
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-12-07 17:03:05 -0300

    feat: add Connection mtd to PostgresDBRepo type

--- internal/repository/dbrepo/postgres_repo.go
+++ internal/repository/dbrepo/postgres_repo.go
@@ -9,6 +9,10 @@ type PostgresDBRepo struct {
 	DB *sql.DB
 }
 
+func (r *PostgresDBRepo) Connection() *sql.DB {
+	return r.DB
+}
+
 func (r *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
 	var movies []*models.Movie
 
