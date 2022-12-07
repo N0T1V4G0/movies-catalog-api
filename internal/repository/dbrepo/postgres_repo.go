f3201365b47c760b84b7476c84c700539b5617d6
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-12-07 16:30:37 -0300

    feat: add PostgresDBRepo type that implements DatabaseRepo interface

---
+++ internal/repository/dbrepo/postgres_repo.go
@@ -0,0 +1,16 @@
+package dbrepo
+
+import (
+	"backend/internal/models"
+	"database/sql"
+)
+
+type PostgresDBRepo struct {
+	DB *sql.DB
+}
+
+func (r *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
+	var movies []*models.Movie
+
+	return movies, nil
+}
