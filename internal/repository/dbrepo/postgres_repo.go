75a4e48a209e3dad921c25c4786cf653f58acc8c
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-12-10 19:48:09 -0300

    feat: finish AllMovies func

--- internal/repository/dbrepo/postgres_repo.go
+++ internal/repository/dbrepo/postgres_repo.go
@@ -2,8 +2,12 @@ package dbrepo
 
 import (
 	"backend/internal/models"
+	"context"
 	"database/sql"
+	"time"
 )
+
+var dbTimeout = time.Second * 3
 
 type PostgresDBRepo struct {
 	DB *sql.DB
@@ -14,7 +18,47 @@ func (r *PostgresDBRepo) Connection() *sql.DB {
 }
 
 func (r *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		SELECT
+			id, title, release_date, runtime,
+			mpaa_rating, description, COALESCE(image, ''),
+			created_at, updated_at
+		FROM
+			movies
+		ORDER BY
+			title
+	`
+
+	rows, err := r.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
 	var movies []*models.Movie
+
+	for rows.Next() {
+		var movie *models.Movie
+		err := rows.Scan(
+			&movie.Id,
+			&movie.Title,
+			&movie.ReleaseDate,
+			&movie.Runtime,
+			&movie.MpaaRating,
+			&movie.Description,
+			&movie.Image,
+			&movie.CreatedAt,
+			&movie.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		movies = append(movies, movie)
+	}
 
 	return movies, nil
 }
