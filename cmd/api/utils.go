d5f12d38112131c1f2ae543254f69ba4eb8dec35
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-12-13 07:09:34 -0300

    feat: add readJSON mtd

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 )
 
@@ -9,6 +11,26 @@ type JSONResponse struct {
 	Error   bool        `json:"error"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxByte := 1024 * 1024 // 1mb
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxByte))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must contain a single JSON value")
+	}
+
+	return nil
 }
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, payload interface{}) error {
